53ef16d512ae5598a4338421aca0a7bc5936c3ec
NafuKrypto <nafisaalam131@gmail.com>
2023-08-02 16:14:56 +0600

    crawl

--- src/main.ts
+++ src/main.ts
@@ -12,37 +12,43 @@ const crawler = new PuppeteerCrawler({
 			"px-4 lg:px-6 sm:basis-7/12 xl:basis-auto",
 			".flex.flex-col.lg:flex-col.px-2.lg:pr-[50px].lg:basis-8/12.xl:basis-9/12.text-center.lg:text-left.lg:justify-start.justify-center.items-center.lg:items-start",
 		];
+		for (const selector of selectors) {
+			await page.waitForFunction(() => {
+				// selectors?.forEach((selector) => {
+				// 	elements = document.querySelectorAll(selector);
+				// });
+				const elements = document.querySelectorAll(selector);
+
+				return elements.length > 0;
+			});
+
+			// const myPromise = Promise.resolve(res);
+			const actorText = await textContent(page, selector);
+			// const myPromise2 = Promise.resolve(actorText);
+			const c = actorText?.toString() || "";
+			fs.appendFile("example.txt", c, (err) => {
+				if (err) {
+					console.error("Error writing to file:", err);
+				} else {
+					console.log("Content written to the file successfully.");
+				}
+			});
+			// await Dataset.pushData({ c });
+			console.log(`ACTOR: ${actorText}`);
+		}
 
 		// await page.waitForSelector(".text-justify");
-		selectors?.forEach((selector) => {
-			const elements = document.querySelectorAll(selector);
-		});
-		await page.waitForFunction(() => {
-			const elements = document.querySelectorAll(".text-justify");
 
-			return elements.length > 0;
-		});
-
-		// const actorText = await page.$eval(".text-justify", (el) => {
-		// 	console.log(el);
-		// 	return el.textContent;
+		// let actorText: any;
+		// selectors?.forEach(async (selector) => {
+		// 	actorText = await page.$$eval(selector, (elements) => {
+		// 		return Array.from(elements).map((element) => element.textContent);
+		// 	});
 		// });
-		const actorText = await page.$$eval(".text-justify", (elements) => {
-			return Array.from(elements).map((element) => element.textContent);
-		});
-		const c = actorText?.toString() || "";
-		fs.appendFile("example.txt", c, (err) => {
-			if (err) {
-				console.error("Error writing to file:", err);
-			} else {
-				console.log("Content written to the file successfully.");
-			}
-		});
-		await Dataset.pushData({ c });
-		console.log(`ACTOR: ${actorText}`);
-		const t = Array.from(document.querySelectorAll("div[.class]")).map(
-			(div: any) => div.class
-		);
+
+		// const t = Array.from(document.querySelectorAll("div[.class]")).map(
+		// 	(div: any) => div.class
+		// );
 	},
 });
 
@@ -52,4 +58,17 @@ const crawler = new PuppeteerCrawler({
 // 		log.info("received body", res.body);
 // 	},
 // });
+const textExtract = async (page: any, selector: any) => {
+	let elements;
+
+	return elements;
+};
+
+const textContent = async (page: any, selector: any) => {
+	const actorText = await page.$$eval(selector, (elements: any) => {
+		console.log(elements);
+		return Array.from(elements).map((element: any) => element.textContent);
+	});
+	return actorText;
+};
 await crawler.run(["http://brotee.org"]);
